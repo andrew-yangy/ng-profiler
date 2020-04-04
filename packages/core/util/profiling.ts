147d94af9b02db9b0bf6051b2f81eece2f4f9d7b
Andrew Yang <andrew.yang@rokt.com>
2020-04-04 22:47:55 +1100

    Goes over dynamic embedded views

--- packages/core/util/profiling.ts
+++ packages/core/util/profiling.ts
@@ -1,11 +1,9 @@
 import { findLView } from "../angular/render3/context_discovery";
 import { getRootView } from "../angular/util/view_traversal_utils";
-import { CONTEXT, HOST, LView, RootContext, TVIEW } from "../angular/interfaces/view";
-import { getComponentLViewByIndex, readPatchedLView } from "../angular/util/view_utils";
-import { RenderFlags } from "../angular/interfaces/definition";
+import { CONTEXT, RootContext } from "../angular/interfaces/view";
+import { readPatchedLView } from "../angular/util/view_utils";
 import { CanvasFactory } from "./canvas";
-import { scheduleOutsideOfZone } from "./zone";
-import { TreeViewFactory, TreeViewItem } from "./treeView";
+import { TreeViewFactory } from "./treeView";
 
 export const startProfiling = () => {
   const view = findLView();
@@ -14,59 +12,11 @@ export const startProfiling = () => {
   const rootComponentLView = readPatchedLView(rootComponent);
 
   CanvasFactory.create();
-  const childComponents = rootComponentLView[TVIEW].components;
 
-  if (childComponents !== null) {
-    attachChildComponents(rootComponentLView, childComponents, TreeViewFactory.setView);
-  }
+  TreeViewFactory.attachComponent(rootComponentLView, TreeViewFactory.setView)
+  console.log(rootComponentLView);
 };
 
 export const stopProfiling = () => {
   console.log('stop');
-};
-
-export const attachChildComponents = (hostLView: LView, components: number[], addChildElement: (children: TreeViewItem[] | TreeViewItem) => void) => {
-  const children = [];
-
-  const addElement = (treeViewItem) => {
-    treeViewItem.parent = hostLView;
-    children.push(treeViewItem);
-  };
-  for (let i = 0; i < components.length; i++) {
-    attachComponent(hostLView, components[i], addElement);
-  }
-  addChildElement(children);
-};
-
-const attachComponent = (hostLView: LView, componentHostIndex: number, addElement: (treeView: TreeViewItem) => void) => {
-  const componentView = getComponentLViewByIndex(componentHostIndex, hostLView);
-  const componentTView = componentView[TVIEW];
-  const treeView = {
-    lView: componentView,
-    currentViewRefIndex: componentHostIndex,
-    children: []
-  };
-  const addChildElement = (res) => {
-    treeView.children = res;
-  };
-  attachTemplate(componentView);
-  const childComponents = componentTView.components;
-  if (childComponents !== null) {
-    attachChildComponents(componentView, childComponents, addChildElement);
-  }
-  addElement(treeView);
-};
-
-export const attachTemplate = (lView: LView) => {
-  const tView = lView[TVIEW];
-  const originTemplate = tView.template;
-  if (!originTemplate) return ;
-  tView.template = (...args) => {
-    originTemplate(...args);
-    if (args[0] === RenderFlags.Update) {
-      scheduleOutsideOfZone(() => {
-        CanvasFactory.draw(lView[HOST].localName, lView[HOST].getBoundingClientRect());
-      })
-    }
-  }
 };
--- packages/core/util/treeView.ts
+++ packages/core/util/treeView.ts
@@ -1,16 +1,22 @@
-import { FLAGS, HOST, LView, LViewFlags } from "../angular/interfaces/view";
+import { CHILD_HEAD, CONTEXT, FLAGS, HOST, LView, LViewFlags, NEXT, PARENT, TVIEW } from "../angular/interfaces/view";
+import { RenderFlags } from "../angular/interfaces/definition";
+import { scheduleOutsideOfZone } from "./zone";
+import { CanvasFactory } from "./canvas";
+import { ACTIVE_INDEX, ActiveIndexFlag, CONTAINER_HEADER_OFFSET, LContainer } from "../angular/interfaces/container";
+import { isLContainer } from "../angular/interfaces/type_checks";
+import { getComponentLViewByIndex } from "../angular/util/view_utils";
 
 export interface TreeViewItem {
   lView: LView;
   children: TreeViewItem[];
-  currentViewRefIndex?: number;
   parent?: TreeViewItem;
-  dynamicEmbeddedViewsChecked?: boolean;
 }
 
 export interface SerializedTreeViewItem {
   name: string;
+  tagName: string;
   children: SerializedTreeViewItem[];
+  parent: string;
   onPush: boolean;
 }
 
@@ -18,19 +24,107 @@ class TreeView {
   serialisedTreeView: SerializedTreeViewItem;
 
   setView = (treeView: TreeViewItem) => {
-    this.serialisedTreeView = this.serialiseTreeViewItem(treeView[0]);
+    this.serialisedTreeView = this.serialiseTreeViewItem(treeView.children[0]);
+    console.log(this.serialisedTreeView);
   };
 
   serialiseTreeViewItem(
     treeViewItem: TreeViewItem
   ): SerializedTreeViewItem {
+    if (!treeViewItem) return ;
     return {
+      name: this.getComponentName(treeViewItem.lView),
+      tagName: treeViewItem.lView && treeViewItem.lView[HOST].localName,
       children: treeViewItem.children && treeViewItem.children.map(loopTreeViewItem =>
         this.serialiseTreeViewItem(loopTreeViewItem)
       ),
-      name: treeViewItem.lView && treeViewItem.lView[HOST].localName,
+      parent: this.getComponentName(treeViewItem.lView[PARENT]),
       onPush: treeViewItem.lView && (treeViewItem.lView[FLAGS] & LViewFlags.CheckAlways) === 0
     };
   }
+
+  getComponentName = (lView: LView | LContainer) => {
+    return lView[CONTEXT].constructor.name;
+  };
+
+  attachChildComponents = (hostLView: LView, components: number[], addChildElement: (children: TreeViewItem[] | TreeViewItem) => void) => {
+    const children = [];
+    const addElement = (treeViewItem) => {
+      children.push(treeViewItem);
+    };
+
+    for (let i = 0; i < components.length; i++) {
+      const componentView = getComponentLViewByIndex(components[i], hostLView);
+      this.attachComponent(componentView, addElement);
+    }
+    addChildElement(children);
+  };
+
+  attachComponent = (componentView: LView, addElement: (treeView: TreeViewItem) => void) => {
+    const componentTView = componentView[TVIEW];
+    const treeView = {
+      lView: componentView,
+      children: []
+    };
+    const addChildElement = (childElements: TreeViewItem[]) => {
+      treeView.children = treeView.children.concat(...childElements);;
+    };
+    this.attachTemplate(componentView);
+    const childComponents = componentTView.components;
+    if (childComponents !== null) {
+      this.attachChildComponents(componentView, childComponents, addChildElement);
+    }
+    this.attachDynamicEmbeddedViews(componentView, addChildElement);
+    addElement(treeView);
+  };
+
+  /**
+   * Goes over dynamic embedded views (ones created through ViewContainerRef APIs) and refreshes
+   * them by executing an associated template function.
+   */
+  attachDynamicEmbeddedViews = (lView: LView, addChildElement) => {
+    let children = [];
+    let viewOrContainer = lView[CHILD_HEAD];
+    while (viewOrContainer !== null) {
+      // Note: viewOrContainer can be an LView or an LContainer instance, but here we are only
+      // interested in LContainer
+      let activeIndexFlag: ActiveIndexFlag;
+      if (isLContainer(viewOrContainer) &&
+        (activeIndexFlag = viewOrContainer[ACTIVE_INDEX]) >> ActiveIndexFlag.SHIFT ===
+        ActiveIndexFlag.DYNAMIC_EMBEDDED_VIEWS_ONLY) {
+
+        for (let i = CONTAINER_HEADER_OFFSET; i < viewOrContainer.length; i++) {
+          const embeddedLView = viewOrContainer[i] as LView;
+          const embeddedTView = embeddedLView[TVIEW];
+
+          embeddedTView.components && this.attachComponent(embeddedLView, (element) => {
+            children = [...children, ...element.children]
+          });
+        }
+        if ((activeIndexFlag & ActiveIndexFlag.HAS_TRANSPLANTED_VIEWS) !== 0) {
+          // We should only CD moved views if the component where they were inserted does not match
+          // the component where they were declared and insertion is on-push. Moved views also
+          // contains intra component moves, or check-always which need to be skipped.
+          // refreshTransplantedViews(viewOrContainer, lView[DECLARATION_COMPONENT_VIEW] !);
+        }
+      }
+      viewOrContainer = viewOrContainer[NEXT];
+    }
+    addChildElement(children);
+  };
+
+  private attachTemplate = (lView: LView) => {
+    const tView = lView[TVIEW];
+    const originTemplate = tView.template;
+    if (!originTemplate) return ;
+    tView.template = (...args) => {
+      originTemplate(...args);
+      if (args[0] === RenderFlags.Update) {
+        scheduleOutsideOfZone(() => {
+          CanvasFactory.draw(lView[HOST].localName, lView[HOST].getBoundingClientRect());
+        })
+      }
+    }
+  };
 }
 export const TreeViewFactory = new TreeView();
