309896cb880590f8d73e5afcc1509e25f8145284
Andrew Yang <andrew.yang@rokt.com>
2020-04-09 17:47:30 +1000

    create split panel

--- packages/devtools-page/src/app/shared/shared.module.ts
+++ packages/devtools-page/src/app/shared/shared.module.ts
@@ -4,22 +4,30 @@ import { TreeDiagramComponent } from './tree-diagram/tree-diagram.component';
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import { SplitPaneComponent } from "./split-pane/split-pane.component";
 
+const MATERIAL_MODULES = [
+  MatTabsModule,
+  MatButtonModule,
+  MatIconModule
+];
 
+const CUSTOM_COMPONENTS = [
+  TreeDiagramComponent,
+  SplitPaneComponent
+];
 
 @NgModule({
-  declarations: [TreeDiagramComponent],
+  declarations: [
+    ...CUSTOM_COMPONENTS
+  ],
   imports: [
     CommonModule,
-    MatTabsModule,
-    MatButtonModule,
-    MatIconModule
+    ...MATERIAL_MODULES
   ],
   exports: [
-    TreeDiagramComponent,
-    MatTabsModule,
-    MatButtonModule,
-    MatIconModule
+    ...CUSTOM_COMPONENTS,
+    ...MATERIAL_MODULES
   ]
 })
 export class SharedModule { }
